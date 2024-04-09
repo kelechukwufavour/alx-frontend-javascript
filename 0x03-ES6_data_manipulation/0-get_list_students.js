6053af45f2834e09d9130267d35c540e102b5a05
kelechukwufavour <kelechukwufavour.faye@gmail.com>
2024-04-09 17:22:32 +0100

    new changes

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,3 @@
-/**
- * Retrieves a list of students.
- * @returns {{id: Number, firstName: String, location: String}[]}
- */
 export default function getListStudents() {
     return [
       { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
