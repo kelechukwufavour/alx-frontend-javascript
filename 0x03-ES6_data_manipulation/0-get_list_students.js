c9c0c9dcd9fd2309c7c9acce6422d323af091cea
kelechukwufavour <kelechukwufavour.faye@gmail.com>
2024-04-09 17:20:55 +0100

    i made corrections to a task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,12 +2,10 @@
  * Retrieves a list of students.
  * @returns {{id: Number, firstName: String, location: String}[]}
  */
-function getListStudents() {
+export default function getListStudents() {
     return [
-        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-        { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 5, firstName: 'Serena', location: 'San Francisco' }
+      { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+      { id: 2, firstName: 'James', location: 'Columbia' },
+      { id: 5, firstName: 'Serena', location: 'San Francisco' },
     ];
-}
-
-export default getListStudents;
+  }
